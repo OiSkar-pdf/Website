5993d8c9d31d23d3b18930d0eba8905d5888604b
Oskar_Iwaniuk <oiwaniuk1116@gmail.com>
2024-04-15 14:38:55 -0400

    message

--- script.js
+++ script.js
@@ -9,18 +9,24 @@ document.addEventListener("DOMContentLoaded", function() {
         const scrollAmount = Math.abs(scrollTop - lastScrollTop);
         const sectionHeight = servicesSection.getBoundingClientRect().height;
         const sectionTop = servicesSection.getBoundingClientRect().top;
-        const halfwayPoint = windowHeight - 690; //adjust for position when scroll starts higher-starts later lower-starts earlier
+        const halfwayPoint = windowHeight - 690; // Adjust for position when scroll starts higher-starts later lower-starts earlier
         const scrollThreshold = sectionHeight + halfwayPoint;
-        const speedAdjust = 3.5; //adjust for speed higher-faster lower-smaller
+        const speedAdjust = 3.5; // Adjust for speed higher-faster lower-smaller
 
         let newPosition = parseFloat(getComputedStyle(servicesSection).left);
 
-        if (scrollDirection === 'down' && sectionTop < scrollThreshold) {
-            newPosition -= scrollAmount * speedAdjust * (sectionHeight / windowHeight);
-        } else if (scrollDirection === 'up' && sectionTop < scrollThreshold) {
-            newPosition += scrollAmount * speedAdjust * (sectionHeight / windowHeight);
-        } else if (scrollDirection === 'up' && sectionTop >= scrollThreshold) {
-            newPosition = 0; // Return to center position
+        // Check if the viewport is in mobile mode (less than or equal to 768px width)
+        if (window.innerWidth <= 768) {
+            newPosition = 0; // Set position to default (no sliding) in mobile view
+        } else {
+            // Adjust position based on scroll behavior for desktop view
+            if (scrollDirection === 'down' && sectionTop < scrollThreshold) {
+                newPosition -= scrollAmount * speedAdjust * (sectionHeight / windowHeight);
+            } else if (scrollDirection === 'up' && sectionTop < scrollThreshold) {
+                newPosition += scrollAmount * speedAdjust * (sectionHeight / windowHeight);
+            } else if (scrollDirection === 'up' && sectionTop >= scrollThreshold) {
+                newPosition = 0; // Return to center position
+            }
         }
 
         servicesSection.style.left = `${newPosition}px`;
